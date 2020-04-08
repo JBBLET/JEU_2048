3d52f27c7f74df1ce5d2d0ee6ec3321b6c7b5c16
Jean-Baptiste BLET <blet.jb@gmail.com>
2020-04-08 14:53:06 +0200

    Modification de la classe damier

--- damier.h
+++ damier.h
@@ -1,11 +1,31 @@
 #ifndef DAMIER_H
 #define DAMIER_H
 
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
 
-class Damier
+class damier
 {
-public:
-    Damier();
+public :
+    damier(int l);
+    ~damier();
+
+    void print();
+    void random(); //Permet d'ajouter de manière aléatoire des 2 et des 4 sur les cases vides de la grille
+
+
+private :
+    int L;
+    int** T;
+    float const p=0.2;
+
+    void alloc(int taille);
+    void free();
+
+
+
 };
 
 #endif // DAMIER_H
