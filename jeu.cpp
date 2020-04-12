092108e1b6f1baa3218c9ee2c456659f884f554e
Jean-Baptiste BLET <blet.jb@gmail.com>
2020-04-12 03:35:25 +0200

    Ecriture des méthodes de Jeu

--- jeu.cpp
+++ jeu.cpp
@@ -1,6 +1,90 @@
 #include "jeu.h"
+#include "damier.h"
 
-Jeu::Jeu()
+
+using namespace std;
+Jeu::Jeu(QObject *parent): QObject(parent)
 {
+    Taille=4;
+    jeuOn=false;
+    gameChanged();
 
+}
+
+void Jeu::start_jeu(QString L="4")
+{   int nouvelle_taille = L.toInt();
+    partie.resize(nouvelle_taille);
+    Taille=nouvelle_taille;
+    partie.random();
+    jeuOn=true;
+    gameChanged();
+}
+QList<QString> Jeu::read_damier()
+{
+    QList<QString> valeurs;
+    for (int i=0;i<Taille;i++)
+    {
+        valeurs.append(QString::number(partie.get_value(i)));
+    }
+    return valeurs;
+}
+QList<bool> Jeu::read_visibilite()
+{
+    QList<bool> visibilite;
+    for (int i=0;i<Taille;i++)
+    {
+        if(partie.get_value(i)==0){ visibilite.append(false);}
+        else {visibilite.append(true);}
+        }
+    return visibilite;
+}
+bool Jeu::test_win()
+{
+    for(int i=0;i<Taille;i++){
+        for(int j=0;j<Taille;j++){
+            int value=partie.get_value(i*Taille+j);
+            if(value==2048){return true;}
+            }
+        }
+    return false;
+}
+void Jeu::test_lose()
+{
+    if(!partie.peut_bouger()){
+        jeuOn=false;
+    }
+
+}
+
+QString Jeu::calcul_score()
+{
+    return QString::number(partie.sum());
+}
+void Jeu::mouvement_bas()
+{if(jeuOn){
+    partie.mouvement_bas();
+    test_lose();
+    gameChanged();
+    }
+}
+void Jeu::mouvement_haut()
+{if(jeuOn){
+    partie.mouvement_haut();
+    test_lose();
+    gameChanged();
+    }
+}
+void Jeu::mouvement_droit()
+{if(jeuOn){
+    partie.mouvement_droit();
+    test_lose();
+    gameChanged();
+    }
+}
+void Jeu::mouvement_gauche()
+{if(jeuOn){
+    partie.mouvement_gauche();
+    test_lose();
+    gameChanged();
+    }
 }
--- jeu.h
+++ jeu.h
@@ -1,6 +1,7 @@
 #ifndef JEU_H
 #define JEU_H
 #include <QObject>
+#include <QString>
 #include <damier.h>
 using namespace std;
 
@@ -8,25 +9,28 @@ class Jeu : public QObject
 {
     Q_OBJECT
 public:
-    explicit Jeu(QObject *parent);
-    Q_INVOKABLE void start_jeu(int L); //debut du jeu dans L on met la taille du damier selectionne par l utilisateurcette fonction est active par un bouton
-    Q_INVOKABLE void mouvement();
-    Q_INVOKABLE int read_damier(int index); //renvoie la valeur de la case reperee par l index
-    Q_INVOKABLE bool read_visibilite(int index);// renvoie la valeur selon si on a besion d afficher la case
-    Q_PROPERTY(int valeur READ read_damier NOTIFY gameChanged)
-    Q_PROPERTY(bool est_visible READ read_visibilite NOTIFY gameChanged)
-    Q_INVOKABLE void restart(int L);
-    void test_win(damier &D);
-    void test_lose(damier &D);
+    explicit Jeu(QObject *parent = nullptr);
+    Q_INVOKABLE void start_jeu(QString L); //debut du jeu dans L on met la taille du damier selectionne par l utilisateur cette fonction est active par un bouton
+    Q_INVOKABLE void mouvement_bas();
+    Q_INVOKABLE void mouvement_haut();
+    Q_INVOKABLE void mouvement_droit();
+    Q_INVOKABLE void mouvement_gauche();
+    QList<QString> read_damier(); //renvoie la valeur de la case reperee par l index
+    QList<bool> read_visibilite();// renvoie la valeur selon si on a besion d afficher la case
+    Q_PROPERTY(QList<QString> valeur READ read_damier NOTIFY gameChanged)
+    Q_PROPERTY(QList<bool> est_visible READ read_visibilite NOTIFY gameChanged)
+    Q_PROPERTY(QString score READ calcul_score NOTIFY gameChanged)
+    bool test_win();
+    void test_lose();
+    QString calcul_score();
 signals:
-    void gamechanged();
+    void gameChanged();
 private slots:
 
 private:
     bool jeuOn;
     damier partie;
     int Taille;
-    int score;
 };
 
 #endif // JEU_H
