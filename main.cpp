86024c39f6038f877975333fada4b1baa6aef23b
alicia barsacq <barsacq.alicia@gmail.com>
2020-04-15 11:02:45 +0200

    Rectification du random : réinitialisation de la séquence ajoutée

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 #include <QQmlContext>
 #include <QObject>
 #include "jeu.h"
+#include <random>
 
 int main(int argc, char *argv[])
 {
+    //Réinitialisation de la séquence aléatoire
+    srand (time(NULL));
+
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
